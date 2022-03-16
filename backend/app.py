4ed243879d11a845754da5ef2ed4fe11e4ab2010
connor <zittrauerconnor@gmail.com>
2022-03-15 19:43:07 -0500

    Database Practice

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,36 @@
-import time
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+
 
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test_database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Prescription(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     name = db.Column(db.String(100))
+     dosage = db.Column(db.Integer)
+     
+     def __init__(self, name, dosage):
+         self.name = name
+         self.dosage = dosage
+
+
+@app.route('/index', methods=['GET'])
+def index():
+    return {
+        'Hello' : 'World'
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {TodoPage} from './Pages/TodoPage'
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
-
-  useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
-
-  return (
-    <div className="App">
-      <header className="App-header">
-
-        ... no changes in this part ...
-
-        <p>The current time: {currentTime}.</p>
-      </header>
-    </div>
-  );
+    return(
+      <div className='App'><TodoPage/></div>
+    )
 }
 
 export default App;
