e48f47a2755067b78bdf0a6b6e677f751d8b0398
connor <zittrauerconnor@gmail.com>
2022-03-16 11:19:02 -0500

    No Such Object

--- backend/app.py
+++ backend/app.py
@@ -1,35 +1,58 @@
+from dataclasses import fields
 import os
-from flask import Flask
+from unicodedata import name
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
-
-
-
-
+from flask_marshmallow import Marshmallow 
 
 app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-
+#this configures the database/joins it with the flask application
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test_database.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
+ma = Marshmallow(app)
+
+#this is a model of the database columns
 class Prescription(db.Model):
      id = db.Column(db.Integer, primary_key=True)
      name = db.Column(db.String(100))
-     dosage = db.Column(db.Integer)
+     dosage = db.Column(db.String(100))
      
      def __init__(self, name, dosage):
          self.name = name
          self.dosage = dosage
 
 
-@app.route('/index', methods=['GET'])
-def index():
+class Prescription_Schema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name', 'dosage')
+prescription_schema = Prescription_Schema()
+prescriptions_schema = Prescription_Schema(many=True)
+
+
+
+@app.route('/get', methods=['GET'])
+def get_prescription():
     return {
         'Hello' : 'World'
     }
+
+
+@app.route('/add', methods=['POST'])
+def add_prescription():
+    name = request.json['name']
+    dosage = request.json['dosage']
+
+    prescriptions = Prescription(name, dosage)
+    db.session.add(prescriptions)
+    db.session.commit()
+
+    return prescription_schema.jsonify(prescriptions)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
