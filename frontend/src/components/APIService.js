a6552af0d9c5f8095e61c2b424a038dca7195f65
Connor <zittrauerconnor@gmail.com>
2022-03-21 09:49:25 -0700

    CORS Error

---
+++ frontend/src/components/APIService.js
@@ -0,0 +1,15 @@
+export default class APIService {
+    static UpdateName(id, name) {
+        return fetch(`http://127.0.0.1:5000/update/${id}/`, {
+            'method':'PUT',
+              headers:{
+                  
+                'Content-Type':'application/json'
+              },
+              
+              body: JSON.stringify(name) 
+    
+          }).then(resp => resp.json)
+    }
+
+}
--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react'
+import APIService from './APIService'
 
 function Form(props) {
     const[name, setName] = useState(props.prescription.name)
     const[dosage, setDosage] = useState(props.prescription.dosage)
 
-
+    const updateName = () => {
+        APIService.UpdateName(props.prescription.id, {name, dosage})
+        .then(resp => console.log(resp))
+        .catch(error => console.log(error))
+    }
   return (
     <div>{props.prescription ? (
 
         <div className="mb-3">
               
-        <label htmlForm="name" className='form-label'>Name</label>
+        <label htmlFor="name" className='form-label'>Name</label>
         
         <input type="text" className="form-control"
         value={name}
         placeholder='Please Enter a Name'
+
+        //this updates the information
         onChange={(e) => setName(e.target.value)}
         />
 
-        <label htmlForm="dosage" className='form-label'>Dosage</label>
+        <label htmlFor="dosage" className='form-label'>Dosage</label>
         <textarea
         rows = "1"
         value={dosage}
@@ -26,6 +33,11 @@ function Form(props) {
         placeholder='Please enter a Dosage'
         onChange={(e) => setDosage(e.target.value)}
         />
+
+        <button
+        onClick={updateName}
+        className='btn-update'
+        >Update</button>
         </div>
 
     ):null}
