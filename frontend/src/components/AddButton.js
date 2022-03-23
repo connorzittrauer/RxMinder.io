e7b3b62cf13a58ddbbd480966d7bdc1b07f92137
Connor <zittrauerconnor@gmail.com>
2022-03-22 18:57:26 -0700

    runnable

--- frontend/src/components/AddButton.js
+++ frontend/src/components/AddButton.js
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from 'react'
+import APIService from './APIService'
 
-function AddButton() {
+
+function AddButton(props) {
+  
+    // const addPrescription = () => {
+    //     APIService.AddPrescription(props.prescription.id, {name, dosage})
+    //     .then(resp => props.updatedData(resp))
+    //     .catch(error=> console.log(error))
+    // }
     return(
     <div>
-        <button>Here is a Button</button>
+        <button
+        // onClick={addPrescription}
+        >Here is a Button</button>
     </div>
     ) 
 }
