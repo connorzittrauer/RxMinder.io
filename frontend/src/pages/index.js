1d9a097a3e97a88608a63ecef9e0d30fd3b878d7
Connor <zittrauerconnor@gmail.com>
2022-04-06 18:36:40 -0500

    rolled back react-version

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -6,17 +6,6 @@ const Home = () => {
 
   const [currentTime, setCurrentTime] = useState(0);
 
-  var CronJob = require('cron').CronJob;
-  var job = new CronJob(
-    '* * * * * ',
-    function() {
-      getTime()
-    },
-    null,
-    true,
-  );
- 
-  
   const getTime = () => {
     APIService.CallFetch('/current_time', 'GET')
     .then(data => {
@@ -24,10 +13,12 @@ const Home = () => {
     })
     .catch(error=> console.log(error))
 }
+
   useEffect(() => {
     getTime()
   }, [])
-  
+
+
   return (
     <div className='page'>
       <h1>Welcome to your Medication Portal</h1>
