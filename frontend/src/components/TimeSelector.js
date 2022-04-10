81ea170cec74219ad2bf20f06c3bb220da41c172
Brianna Conly <briannafatheree@student.bpcc.edu>
2022-04-09 20:08:23 -0500

    delete times, edit times, add new times, update times, styling buttons in update view

--- frontend/src/components/TimeSelector.js
+++ frontend/src/components/TimeSelector.js
@@ -1,21 +1,29 @@
 import React, { useState, useEffect} from 'react'
 
+
 const TimeSelector = (props) => {
     
     const[hour, setHour] = useState(props.hour)
     const[min, setMin] = useState(props.min)
     const[meridiem, setMer] = useState(props.meridiem)
-    console.log(props)
+
     useEffect(() => {
-        props.setTime(hour + ":" + min)
-        props.setMer(meridiem)
-    }, [hour, min, meridiem, props]) // when hour, min, or meridiem change set the time, added props to be watched to get rid of warning
+        props.setTime(hour + ":" + min, props.id)
+        props.setMer(meridiem,  props.id)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [hour, min, meridiem]) // when hour, min, or meridiem change set the time
+    
+    useEffect(() => {
+        setHour(props.hour)
+        setMin(props.min)
+        setMer(props.meridiem)
+    }, [props.hour, props.min, props.meridiem])
 
     // making opts array of options for hours
     const hourOpts = () => {
         let opts = []
         for(let i = 1; i <= 12; i++) {
-        opts.push(<option value={i} key={`hour${i}`}>{i}</option>) // adding a key to components in an array of components makes console quiet-er
+            opts.push(<option value={i} key={`hour${i}`}>{i}</option>) // adding a key to components in an array of components makes console quiet-er
         }
         return opts
     }
@@ -24,12 +32,12 @@ const TimeSelector = (props) => {
         let m
         let opts = []
         for(let i = 0; i <= 55; i = i + 5) {
-        if(i < 10) {
-            m = "0" + i
-        } else {
-            m = i
-        }
-        opts.push(<option value={m} key={`min${m}`}>{m}</option>)
+            if(i < 10) {
+                m = "0" + i
+            } else {
+                m = i
+            }
+            opts.push(<option value={m} key={`min${m}`}>{m}</option>)
         }
         return opts
     }
@@ -47,6 +55,7 @@ const TimeSelector = (props) => {
                 <option value="am">AM</option>
                 <option value="pm">PM</option>
             </select>
+            { props.edit ? (<img style={{marginBottom:"5px"}} alt="" src="./deleteicon.png" onClick={() => props.delete(props.id)}/>): null }
         </div>
     )
 }
