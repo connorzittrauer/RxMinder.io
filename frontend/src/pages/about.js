7543b90b4a4714f478622630f171236521de30bc
Connor <zittrauerconnor@gmail.com>
2022-04-12 10:29:02 -0500

    unmounted component error

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -8,28 +8,31 @@ const About = () => {
 
   const schedule = require('node-schedule');
   const {currentTime} = useContext(TimeMonitorContext)
-  const [rxTimes, getPrescriptionTimes] = useState([])
-  
+  const [rxTimes, getPrescriptionTimes] = useState(0)
+ 
+
   const getRxTimes = () => {
     APIService.CallFetch('/times', 'GET')
     .then(data => {
       getPrescriptionTimes(data)
     })
-
+    .then(console.log(rxTimes[0]))
     .catch(error=> console.log(error))
   }
 
   const checkTimes = () => {
     for (let i = 0; i < rxTimes.length; i++){
       console.log(rxTimes[i].time + ' ' + rxTimes[i].meridiem)
-      console.log(currentTime === (rxTimes[i].time + ' ' + rxTimes[i].meridiem))
+      console.log(currentTime == (rxTimes[i].time + ' ' + rxTimes[i].meridiem))
     }
   }
+
    useEffect(() => {
     const job = schedule.scheduleJob('*/1 * * * *', function(){
-      getRxTimes()
-      checkTimes()
+     getRxTimes()      
+     checkTimes()
     });
+    
   }, [])  
   
   return (
